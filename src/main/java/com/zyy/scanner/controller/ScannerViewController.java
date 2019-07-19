101c0bd29f33bede25f70519b57822af94aaf7d4
yiyang <a56607529@163.com>
2019-07-19 10:34:06 +0800

    扫描

--- src/main/java/com/zyy/scanner/controller/ScannerViewController.java
+++ src/main/java/com/zyy/scanner/controller/ScannerViewController.java
@@ -3,7 +3,9 @@ package com.zyy.scanner.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import io.swagger.annotations.ApiOperation;
@@ -13,10 +15,15 @@ import io.swagger.annotations.ApiOperation;
  * @DateTime 2019-07-19 09:24
  * @Description
  */
-@Controller
+@RestController
 @RequestMapping("/view")
 public class ScannerViewController {
 
+    @ApiOperation(value = "Scanner主页")
+    @RequestMapping(value="/page",method=RequestMethod.GET)
+    public ModelAndView view(){
+        return new ModelAndView("/webjars/scanner-ui/0.0.1-SNAPSHOT/index.jsp");
+    }
 
     @ApiOperation(value="测试")
     @PostMapping("/test")
